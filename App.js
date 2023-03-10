b03ed99f778859cba5a09a83be29552d67cc3af2
LePotassio <furukawa.em@gmail.com>
2023-03-10 03:57:19 -0800

    Button implemented, push before build

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
+import { CreativeKit } from '@snapchat/snap-kit-react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView, Dimensions, Button, TouchableOpacity, Alert } from 'react-native';
 
 /*
 Eric Furukawa
@@ -14,17 +15,44 @@ https://stackoverflow.com/questions/43212931/react-native-horizontal-scroll-view
 const { width } = Dimensions.get('window');
 
 export default function App() {
+  const sharePicture = (url) => {
+    CreativeKit.sharePhoto({
+      content: {
+        uri: 'https://picsum.photos/200',
+      },
+     caption: "caption string",
+    })/*
+    .catch((error) => {
+      Alert.alert(error);
+    })*/;
+  };
+
+  const doAlert = (alert) => {
+    Alert.alert(alert);
+  };
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <Text style={styles.titleText}>Snapchat Application</Text>
       <ScrollView ref={(scrollView) => {this.scrollView=scrollView}} style={styles.scrollView} horizontal={true} decelerationRate={0} snapToInterval={width-50} snapToAlignment={"center"} contentInset={{top: -50, left:30, bottom: -50, right: 30}}>
         <View style={styles.box}>
           <Image style={styles.images} source={require('./assets/Photos/Soup.jpg')}></Image>
+          <TouchableOpacity style={styles.button} onPress={() => { sharePicture(); }}>
+            <Text style={styles.buttonText}>Share</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.box}></View>
-        <View style={styles.box}></View>
-        <View style={styles.box}></View>
-        <View style={styles.box}></View>
+        <View style={styles.box}>
+          <Image style={styles.images} source={require('./assets/Photos/Soup.jpg')}></Image>
+        </View>
+        <View style={styles.box}>
+          <Image style={styles.images} source={require('./assets/Photos/Soup.jpg')}></Image>
+        </View>
+        <View style={styles.box}>
+          <Image style={styles.images} source={require('./assets/Photos/Soup.jpg')}></Image>
+        </View>
+        <View style={styles.box}>
+          <Image style={styles.images} source={{uri: 'https://picsum.photos/200'}}></Image>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -48,18 +76,17 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   titleText: {
-    top: 10,
+    top: 20,
     left: 10,
     fontSize: 30,
     fontWeight: 'bold',
     color: '#ffffff',
   },
   images: {
+    flex: 5,
     width: '90%',
     resizeMode: 'contain',
-    bottom: 170,
     alignSelf: 'center',
-    borderRadius: 100,
     overflow: 'hidden',
   },
   box: {
@@ -69,6 +96,25 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 600,
     borderRadius: 40,
+    justifyContent: 'center',
     overflow: 'hidden',
+  },
+  button: {
+    marginBottom: 20,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    width: '90%',
+    borderColor: 'yellow',
+    borderWidth: 5,
+    borderRadius: 1,
+    alignSelf: 'center',
+    borderRadius: 30,
+  },
+  buttonText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
   }
 });
